c7d7b94a8c6fa3fc3c540d58675d53cf923c7bcf
hyeongls <lshyeong47@naver.com>
2026-08-03 16:16:42 +0900

    KAN-131 feat: fetchFamily 함메서드 개발

--- core/src/main/java/com/example/project/consultation/dto/fastapi/FamilyData.java
+++ core/src/main/java/com/example/project/consultation/dto/fastapi/FamilyData.java
@@ -2,10 +2,14 @@ package com.example.project.consultation.dto.fastapi;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 import java.time.LocalDate;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Builder
+@AllArgsConstructor
 public record FamilyData(
         Long familyId,
         String name,
