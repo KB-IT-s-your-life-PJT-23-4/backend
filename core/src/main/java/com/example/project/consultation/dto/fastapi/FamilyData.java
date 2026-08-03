86cb1d7329118b1ece62db921df37d121ea4b455
hyeongls <lshyeong47@naver.com>
2026-08-03 17:08:35 +0900

    KAN-131 refactor: FamilyData record -> class

--- core/src/main/java/com/example/project/consultation/dto/fastapi/FamilyData.java
+++ core/src/main/java/com/example/project/consultation/dto/fastapi/FamilyData.java
@@ -4,23 +4,26 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDate;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Getter
 @Builder
 @AllArgsConstructor
-public record FamilyData(
-        Long familyId,
-        String name,
-        String relationshipType,
-        Long giftAmount,
-        Integer recipientAge,
-        Boolean recipientIsMinor,
-        Boolean hasPreviousGifts,
-        Long previousGiftAmount,
-        LocalDate previousGiftDate,
-        Boolean previousGiftSameDonor,
-        Long previouslyUsedDeduction,
-        LocalDate deductionRenewalDate
-) {}
+public class FamilyData {
+
+    private final Long familyId;
+    private final String name;
+    private final String relationshipType;
+    private final Long giftAmount;
+    private final Integer recipientAge;
+    private final Boolean recipientIsMinor;
+    private final Boolean hasPreviousGifts;
+    private final Long previousGiftAmount;
+    private final LocalDate previousGiftDate;
+    private final Boolean previousGftSameDonor;
+    private final Long previouslyUsedDeduction;
+    private final LocalDate deductionRenewalDate;
+}
