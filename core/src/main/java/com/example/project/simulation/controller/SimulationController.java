cbf9149ddeefcabafc03e0e637ae85f64aef6e27
wosyh18 <wosyh18@naver.com>
2026-08-05 21:24:53 +0900

    feat: 시뮬레이션 최종 저장 controller 구현

--- core/src/main/java/com/example/project/simulation/controller/SimulationController.java
+++ core/src/main/java/com/example/project/simulation/controller/SimulationController.java
@@ -23,9 +23,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -180,7 +180,7 @@ public class SimulationController {
                 ));
     }
 
-    @PutMapping("/{simulationId}/save")
+    @PatchMapping("/{simulationId}/save")
     public ApiResponse<SimulationSaveResponse> save(
             @PathVariable String simulationId,
             @Valid @RequestBody SimulationSaveRequest request,
