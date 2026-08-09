fcfb628ea57743ad403346e196b16154010ba15d
hyeongls <lshyeong47@naver.com>
2026-08-09 20:51:52 +0900

    KAN-227 refact: 관리자 계정 삭제로 변경

--- core/src/main/java/com/example/project/admin/auth/controller/AdminAuthController.java
+++ core/src/main/java/com/example/project/admin/auth/controller/AdminAuthController.java
@@ -59,7 +59,7 @@ public class AdminAuthController {
             @Min(0)
             Integer page,
 
-            @RequestParam(defaultValue = "20")
+            @RequestParam(defaultValue = "10")
             @Min(1)
             @Max(Pagination.MAX_PAGE_SIZE)
             int size,
