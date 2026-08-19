afa1a59c1b14ecaac276508d2b7b9fe1e2f76286
jyk3716 <jyk3716715@gmail.com>
2026-08-20 03:26:42 +0900

    KAN-303 feature: 배치 관련 설정 수정

--- batch/src/main/java/com/example/project/batch/config/BatchConfig.java
+++ batch/src/main/java/com/example/project/batch/config/BatchConfig.java
@@ -1,13 +1,28 @@
 package com.example.project.batch.config;
 
+import com.example.project.batch.pii.PiiMigrationConfig;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @EnableBatchProcessing
-@ComponentScan(basePackages = "com.example.project.batch")
+@ComponentScan(
+        basePackages = "com.example.project.batch",
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = PiiMigrationConfig.class
+        )
+)
 @PropertySource({"classpath:batch.properties", "classpath:database.properties"})
 public class BatchConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 }
